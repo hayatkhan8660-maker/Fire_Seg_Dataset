eb7daf1976d8032ce0f7e8a693e6afacd047a440
Hayat Ullah <36618567+hayatkhan8660-maker@users.noreply.github.com>
2022-09-14 19:12:21 -0500

    Update test.py

--- test.py
+++ test.py
@@ -12,9 +12,8 @@ ap.add_argument("-tm", "--test_mode", required=True,
 args = vars(ap.parse_args())
 
 model = shufflnet_unet(n_classes=2,  input_height=416, input_width=608)
-model.load_weights("F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/unet/shufflenet_unet11/shufflenet_unet11_.h5")
+model.load_weights("F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/trained_weights/shufflenet_unet11_.h5")
 print("Loaded model from disk")
-#model=model_from_checkpoint_path("F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/unet/shufflenet_unet11/shufflenet_unet11_")
 #Display the model's architecture
 model.summary()
 
@@ -22,14 +21,14 @@ model.summary()
 if args["test_mode"] == "multiple":
   predict_multiple(
   model, 
-  checkpoints_path="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/unet/shufflenet_unet11/shufflenet_unet11_", 
+  checkpoints_path="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/trained_weights/shufflenet_unet11_", 
   inp_dir="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/new_test_images/", 
   out_dir="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/shufflenet_unet_results/shufflenet_unet12/")
 
 elif args["test_mode"] == "video":
   predict_video(
   model,
-  checkpoints_path="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/unet/shufflenet_unet11/shufflenet_unet11_", 
+  checkpoints_path="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/trained_weights/shufflenet_unet11_", 
   inp="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/new_test_images/fire_video.mp4", 
   output="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/shufflenet_unet_results/shufflenet_unet12/fire_seg_video.mp4")
 else:
