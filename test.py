23f63471dd19950476867912823c5bd31967485f
Hayat Ullah <36618567+hayatkhan8660-maker@users.noreply.github.com>
2022-09-14 19:18:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ ap.add_argument("-tm", "--test_mode", required=True,
 args = vars(ap.parse_args())
 
 model = shufflnet_unet(n_classes=2,  input_height=416, input_width=608)
-model.load_weights("F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/trained_weights/shufflenet_unet11_.h5")
+model.load_weights("path to the trained model weights with (.h5) extension")
 print("Loaded model from disk")
 #Display the model's architecture
 model.summary()
@@ -21,21 +21,21 @@ model.summary()
 if args["test_mode"] == "multiple":
   predict_multiple(
   model, 
-  checkpoints_path="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/trained_weights/shufflenet_unet11_", 
-  inp_dir="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/new_test_images/", 
-  out_dir="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/shufflenet_unet_results/shufflenet_unet12/")
+  checkpoints_path="path to the trained model checpoints", 
+  inp_dir="path to the input fire images directory", 
+  out_dir="path to the output segmented fire images directory")
 
 elif args["test_mode"] == "video":
   predict_video(
   model,
-  checkpoints_path="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/trained_weights/shufflenet_unet11_", 
-  inp="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/new_test_images/fire_video.mp4", 
-  output="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/shufflenet_unet_results/shufflenet_unet12/fire_seg_video.mp4")
+  checkpoints_path="path to the trained model checpoints", 
+  inp="path to the input test video", 
+  output="path to the output fire segmented video")
 else:
   predict(
     model,
-    checkpoints_path="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/unet/shufflenet_unet11/shufflenet_unet11_", 
-    inp="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/new_test_images/fire007.png",
-    out_fname="F:/Hayat Data/Hayat master's PC backup/drive volume G/image-segmentation-keras-master/fire_segmentation/shufflenet_unet_results/shufflenet_unet12/fire007.png")
+    checkpoints_path="path to the trained model checpoints", 
+    inp="path to the input fir image",
+    out_fname="path to the output fire segmented image")
 
 
